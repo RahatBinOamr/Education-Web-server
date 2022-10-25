55c05569acadc71ce5aa1f325f3b77970ca9ca98
RahatBinOamr <108364543+RahatBinOamr@users.noreply.github.com>
2022-10-25 15:40:59 +0600

    create courses file and create courses api data

--- index.js
+++ index.js
@@ -5,18 +5,41 @@ app.use(cors());
 
 const Port = process.env.Port || 5000;
 const category = require("./Data/Category.json")
+const courses = require('./Data/Courses.json')
+
+
+app.get('/courses',(req,res)=>{
+  res.send(courses);
+})
+
+
+app.get('/courses/:id',(req,res)=>{
+  const id = req.params.id;
+  const selectedCourses = courses.find(n=>n.id===id);
+  res.send(selectedCourses)
+})
 
 app.get("/CoursesCategory",(req,res)=>{
-    res.send(category)
+  res.send(category)
 })
-app.get("/CoursesCategory/:id", (req, res) => {
-    const id = req.params.id;
-    const getSingleItem = category?.find((p) => p.id == id);
-    if (!getSingleItem) {
-      res.send("no more id");
-    }
-    res.send(getSingleItem);
-  });
+
+app.get('/CoursesCategory/:id',(req,res)=>{
+  const id = req.params.id;
+  if(id=="07"){
+    res.send(courses)
+  }
+  else{
+  
+    const categoryCourses = courses.filter(n=>n.category_id===id);
+    res.send(categoryCourses)
+  }
+  })
+
+
+
+
+
+
 app.get("/",(req,res)=>{
     res.send("Now sever running");
 })
