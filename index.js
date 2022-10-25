911e285aabcf2eda94ad38ff2b335007469e1de8
RahatBinOamr <108364543+RahatBinOamr@users.noreply.github.com>
2022-10-25 12:29:10 +0600

    create category api and categoriId api

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 const express = require("express");
 const app = express();
 const cors = require ("cors");
-app.use(cors);
+app.use(cors());
+
+const Port = process.env.Port || 5000;
+const category = require("./Data/Category.json")
+
+app.get("/CoursesCategory",(req,res)=>{
+    res.send(category)
+})
+app.get("/CoursesCategory/:id", (req, res) => {
+    const id = req.params.id;
+    const getSingleItem = category?.find((p) => p.id == id);
+    if (!getSingleItem) {
+      res.send("no more id");
+    }
+    res.send(getSingleItem);
+  });
+app.get("/",(req,res)=>{
+    res.send("Now sever running");
+})
+app.listen(Port,()=>{
+    console.log("server is running ", Port);
+});
